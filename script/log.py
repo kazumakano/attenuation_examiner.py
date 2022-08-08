7074d674330d2d82ffc3bcc179c0cc1155bdfd1a
kazumakano <kazumakano@outlook.jp>
2022-08-08 15:58:55 +0900

    several updates

--- script/log.py
+++ script/log.py
@@ -1,7 +1,9 @@
 import csv
 import os.path as path
 from datetime import datetime
+from statistics import multimode
 import numpy as np
+import pandas as pd
 from particle_filter.script.log import Log as PfLog
 
 
@@ -22,3 +24,22 @@ class Log(PfLog):
 
         print(f"log.py: {path.basename(file)} has been loaded")
         print(f"log.py: log begins at {self.ts[0]} and ends at {self.ts[-1]}")
+
+    def get_rssi_info_list(self) -> None:
+        if not hasattr(self, "vis_mac_list"):
+            raise Exception("log.py: log visualizer hasn't been initialized yet")
+
+        rssi_info_list = np.empty((len(self.vis_mac_list), 3), dtype=np.float32)
+        for i in range(len(self.vis_mac_list)):
+            rssi_info_list[i][0] = np.mean(multimode(self.vis_rssi_list[i]))
+            rssi_info_list[i][1] = np.median(self.vis_rssi_list[i])
+            rssi_info_list[i][2] = np.mean(self.vis_rssi_list[i])
+
+        rssi_info_list = pd.DataFrame({
+            "mac address": self.vis_mac_list,
+            "mode": rssi_info_list[:, 0],
+            "median": rssi_info_list[:, 1],
+            "mean": rssi_info_list[:, 2]
+        })
+
+        return rssi_info_list
